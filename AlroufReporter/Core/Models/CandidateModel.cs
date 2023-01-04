2da00deb39c1c3baadc37855e86512674dcfef6a
Tenmayos <tenmayos_2@hotmail.com>
2023-01-04 12:14:03 +0300

    Added `CandidateModel` to act as a datatype model for the retrieved data, included ResumeFile incase a need rises in the future.

---
+++ AlroufReporter/Core/Models/CandidateModel.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AlroufReporter.Core.Models
+{
+    public class CandidateModel
+    {
+        // In a real world App, it is best to clear up the data and follow common code conventions, in this case properties should be PascalCase
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string fullName { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string school { get; set; }
+        public string major { get; set; }
+        public string gpa { get; set; }
+        public string totalGpa { get; set; }
+        public bool doneVolunteerWork { get; set; }
+        public ResumeFile ResumeFile { get; set; }
+        public int __v { get; set; }
+    }
+}
